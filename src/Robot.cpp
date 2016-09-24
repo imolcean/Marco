60d0245afc728c47f461b902f610d99da6dc3f30
Igor Molcean <igor.molcean@gmail.com>
2016-09-24 16:27:23 +0200

    LMY algorithm integrated. Timer still not implemented.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -7,6 +7,7 @@
 
 #include "Robot.h"
 #include <iostream>
+#include <math.h>
 
 namespace marco
 {
@@ -26,8 +27,86 @@ Track& Robot::getRightTrack()
 
 void Robot::move(double x, double y, int time)
 {
-	// TODO LMY Algorithm
-	std::cout << "ROBOT MOVE: " << x << ", " << y << ", " << time << std::endl;
+	// TODO Timer
+
+	// Velocity of the tracks
+	double vLeft;
+	double vRight;
+
+	// Dead zone close to zero
+	if(fabs(x) < 0.1)
+	{
+		x = 0;
+	}
+
+	if(fabs(y) < 0.1)
+	{
+		y = 0;
+	}
+
+	// Normal speed is the length of the vector
+	// Turn speed is the speed of the inner track
+	double vNorm = sqrt(pow(x, 2) + pow(y, 2));
+	double vTurn = vNorm - fabs(y);
+
+	// Moving backwards
+	if(x < 0)
+	{
+		vNorm = -vNorm;
+		vTurn = -vTurn;
+	}
+
+	// Turning around
+	//
+	// When x is zero and y is not
+	// robot will turn around it's center
+	// and not around the track
+	//
+	// TODO Make configurable
+	{
+		if(x == 0)
+		{
+			if(y != 0)
+			{
+				vTurn = -vNorm;
+			}
+		}
+	}
+
+
+	// Determine direction of the turn
+	if(y < 0)
+	{
+		vLeft = vNorm;
+		vRight = vTurn;
+	}
+	else
+	{
+		vLeft = vTurn;
+		vRight = vNorm;
+	}
+
+	// Normalize the values
+	//
+	// If values are bigger than 1.0
+	// they will be scaled so the
+	// biggest one will be 1.0
+	//
+	// TODO Make configurable
+	{
+		double t = std::max(fabs(vLeft), fabs(vRight));
+
+		if(t > 1)
+		{
+			vLeft = vLeft / t;
+			vRight = vRight / t;
+		}
+	}
+
+	m_leftTrack.setVelocity(vLeft);
+	m_rightTrack.setVelocity(vRight);
+
+	std::cout << "ROBOT MOVE: " << vLeft << ", " << vRight << ", " << time << std::endl;
 }
 
 void Robot::stop()
